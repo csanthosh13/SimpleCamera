c100f054b5bd8db6fd689df49a5d487147b2be59
Santhosh Chandrasekharan <csanthosh13@gmail.com>
2019-03-04 23:16:55 +0530

    Initial Commit

---
+++ SimpleCamera/PhotoViewController.swift
@@ -0,0 +1,34 @@
+//
+//  PhotoViewController.swift
+//  SimpleCamera
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    var image:UIImage?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        imageView.image = image
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: - Action methods
+    
+    @IBAction func save(sender: UIButton) {
+        guard let imageToSave = image else {
+            return
+        }
+        UIImageWriteToSavedPhotosAlbum(imageToSave, nil, nil, nil)
+        dismiss(animated: true, completion: nil)
+    }
+
+}
